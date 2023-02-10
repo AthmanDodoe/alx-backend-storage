9f833f7678cba6be7f10b6c64b4cf8593a4213e1
AthmanDodoe <jilloathman943@gmail.com>
2023-02-10 21:48:30 +0300

    0x00. MySQL advanced

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,3 @@
--- Creates an index idx_name_first_score on table names and first letter of name and score
--- Only first letter of name AND score must be indexed
-CREATE INDEX idx_name_first_score
-ON names (LEFT(name, 1), score);
+-- This SQL script creates an index named 'idx_name_first_score' on the 'name' and 'score' columns of the 'names' table. The index will be created on the first character of the name column, and the score column, so it allows for faster search and sorting operation on those columns.
+
+CREATE INDEX idx_name_first_score ON names(name(1), score);
